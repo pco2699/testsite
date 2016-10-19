f2ad8203bc68c12c194f34bde14f99094f0cfb51
pco2699 <pco2699@ks-macbook-pro.local>
2016-10-19 23:55:05 +0900

    Delete log file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.logger = Logger.new('debug.log', 'daily')
+  config.logger = Logger.new('log/debug.log', 'daily')
 end
